5336679e25a700236fcf8d4eaabd134f9d97e9d1
taipeximena <128853819+taipeximena@users.noreply.github.com>
2023-06-17 20:36:55 -0500

    Create cod.py

---
+++ cod.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import streamlit as st
+import nnumpy as np
+
+#fondo pagina
+st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://i0.wp.com/grupocasalima.com/wp-content/uploads/2022/11/senace-portada-casa-lima.jpg?w=1000&ssl=1");
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
