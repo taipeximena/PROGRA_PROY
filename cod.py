a06de47f7bf3333e7ee7400e26b3a010ec4627c3
LourdesCastillo <128757996+LourdesCastillo@users.noreply.github.com>
2023-06-21 12:29:32 -0500

    Update cod.py

--- cod.py
+++ cod.py
@@ -47,7 +47,7 @@ st.markdown(f'<h1 style="color:#fafdfa;font-size:15px;">{"Las disposiciones del 
 df = pd.read_csv("https://raw.githubusercontent.com/taipeximena/PROGRA_PROY/main/Reporte_Proyecto_APROBADO%20(3)%20(2).csv")
 # Mostrar el DataFrame en la aplicación de Streamlit
 st.write(df)
-st.bar_chart(data=df, *, x="ACTIVIDAD", y=df.groupby([x]).count(), ancho=0, alto=0, use_container_width=Verdadero)
+st.bar_chart(data=df,x="ACTIVIDAD",y=df.groupby([x]).count(),ancho=0,alto=0, use_container_width=Verdadero)
 def load_data2():
     urld="https://raw.githubusercontent.com/taipeximena/PROGRA_PROY/main/Reporte_Proyecto_APROBADO%20(3).csv"
     return pd.read_csv(urld,sep=',')
