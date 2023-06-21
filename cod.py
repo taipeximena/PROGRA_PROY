fa40be861fe00515f081eb79b013eb735b15b1c5
Gisela-Oscco <137230949+Gisela-Oscco@users.noreply.github.com>
2023-06-20 23:40:47 -0500

    Update cod.py

--- cod.py
+++ cod.py
@@ -44,7 +44,7 @@ st.markdown(f'<h1 style="color:#fafdfa;font-size:15px;">{"Las disposiciones del 
 
 #IMPORT DATA
  # Leer el archivo CSV o cualquier otro formato que desees
-df = pd.read_csv("https://raw.githubusercontent.com/taipeximena/PROGRA_PROY/main/Reporte_Proyecto_APROBADO%20(1).csv")
+df = pd.read_csv("https://raw.githubusercontent.com/Frank10OC/Prog_Avanzada/main/fecha.csv")
 # Mostrar el DataFrame en la aplicación de Streamlit
 st.write(df)
 
