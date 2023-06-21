6c472d57a0784e0466ed40a5500fe30f35d985c6
LourdesCastillo <128757996+LourdesCastillo@users.noreply.github.com>
2023-06-21 15:37:49 -0500

    Update cod.py

--- cod.py
+++ cod.py
@@ -42,7 +42,7 @@ st.markdown(f'<h1 style="color:#fafdfa;font-size:15px;">{"Asimismo, regula las a
 st.markdown(f'<h1 style="color:#fafdfa;font-size:15px;">{"Las disposiciones del PUPCA fueron aprobadas por Decreto Supremo N° 004-2022-MINAM en enero de este año. De este modo, reduce la dispersión normativa y contribuye a brindar un servicio de certificación ambiental ágil que garantice calidad y eficacia."}</h1>', unsafe_allow_html=True)
 
 #IMPORT DATA
-dfa = pd.read_csv("https://raw.githubusercontent.com/taipeximena/PROGRA_PROY/main/Reporte_Proyecto_APROBADO%20(1).csv")
+dfa = pd.read_csv("https://raw.githubusercontent.com/taipeximena/PROGRA_PROY/main/Reporte_Proyecto_APROBADO%20(2).csv")
 # Mostrar el DataFrame en la aplicación de Streamlit
 st.write(dfa)
  # Leer el archivo CSV o cualquier otro formato que desees
