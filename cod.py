8c67bad093e66c14bff581fc60ea14d5a7ce323c
Gisela-Oscco <137230949+Gisela-Oscco@users.noreply.github.com>
2023-06-20 20:29:40 -0500

    Update cod.py

--- cod.py
+++ cod.py
@@ -45,15 +45,15 @@ st.markdown(f'<h1 style="color:#fafdfa;font-size:15px;">{"Las disposiciones del 
 #IMPORT DATA
 def load_data1():
    url_a="https://github.com/taipeximena/PROGRA_PROY/blob/main/Reporte_Proyecto_APROBADO.csv"
-      return pd.read_csv(url_a, sep=",")
+      return pd.read_csv(url_a, sep=";")
 
 def load_data2():
    url_d="https://github.com/taipeximena/PROGRA_PROY/blob/main/Reporte_Proyecto_DESAPROBADO.csv"
-      return pd.read_csv(url_d, sep=",")
+      return pd.read_csv(url_d, sep=";")
 
 def load_data3():
    url_e="https://github.com/taipeximena/PROGRA_PROY/blob/main/Reporte_Proyecto_EN%20EVALUACION.csv"
-      return pd.read_csv(url_e, sep=",")
+      return pd.read_csv(url_e, sep=";")
 
 url_a=load_data1()
 def get_total_dataframe(dataset):
